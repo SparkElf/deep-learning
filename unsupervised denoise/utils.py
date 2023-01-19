b3c47cf82a89a9cfabf357dc4025f1b8323b63b4
SparkElf <374656045@qq.com>
2023-01-19 18:41:48 +0800

    code

--- unsupervised denoise/utils.py
+++ unsupervised denoise/utils.py
@@ -1,18 +1,33 @@
 import math
-from torch import Tensor, cat, zeros, ones,  randperm, bernoulli, full
-from zmq import device
+from torch import tensor, cat, zeros, ones,  randperm, bernoulli, full
+from torch.nn.functional import conv2d
 
 
 # percent:percent of zeros in mask
-def create_mask(height=128, width=128, percent=0.3, probability=0.5, mode='percent', device='cpu'):
+def create_mask(channels=3, height=512, width=512, percent=0.3, probability=0.2, mode='bernoulli', device='cuda'):
     if mode == 'percent':
-        num = width*height
+        num = width*height*channels
         num_zeros = math.floor(num*percent)
         num_ones = num-num_zeros
-        x = cat((zeros(num_zeros, device=device), ones(
-            num_ones, device=device)))
+        x = cat((zeros(num_zeros, device=device), ones(num_ones, device=device)))
         x = x[randperm(num)]
-        x = x.view((height, width))
+        x = x.view((channels, height, width))
         return x
     elif mode == 'bernoulli':
-        return bernoulli(full((height, width), probability, device=device))
+        return bernoulli(full((channels, height, width), 1-probability, device=device))
+
+
+def image_gradient(img, mode='sobel', device='cuda'):
+    if mode == 'weak':
+        kernel_x = tensor(data=[[0., 0., 0.], [0., -1., 1.], [0., 0., 0.]],
+                          device=device).repeat(3, 3, 1, 1)
+        kernel_y = tensor(data=[[0., 0., 0.], [0., -1., 0.], [0., 1., 0.]],
+                          device=device).repeat(3, 3, 1, 1)
+    elif mode == 'sobel':
+        kernel_x = tensor(data=[[-1., 0., 1.], [-2., 0., 2.], [-1., 0., 1.]],
+                          device=device).repeat(3, 3, 1, 1)
+        kernel_y = tensor(data=[[1., 2., 1.], [0., 0., 0.], [-1., -2., -1.]],
+                          device=device).repeat(3, 3, 1, 1)
+    dx = conv2d(img, weight=kernel_x, padding=1)
+    dy = conv2d(img, weight=kernel_y, padding=1)
+    return 0.5*(dx+dy)
