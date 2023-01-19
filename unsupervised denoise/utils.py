e38524fe4fea5d0e450259ce6c1f03144dcd005a
SparkElf <374656045@qq.com>
2023-01-19 17:00:22 +0800

    code

--- unsupervised denoise/utils.py
+++ unsupervised denoise/utils.py
@@ -4,7 +4,7 @@ from zmq import device
 
 
 # percent:percent of zeros in mask
-def create_mask(channels=3, height=128, width=128, percent=0.3, probability=0.5, mode='percent', device='cpu'):
+def create_mask(height=128, width=128, percent=0.3, probability=0.5, mode='percent', device='cpu'):
     if mode == 'percent':
         num = width*height
         num_zeros = math.floor(num*percent)
@@ -12,8 +12,7 @@ def create_mask(channels=3, height=128, width=128, percent=0.3, probability=0.5,
         x = cat((zeros(num_zeros, device=device), ones(
             num_ones, device=device)))
         x = x[randperm(num)]
-        x = x.view((height, width)).unsqueeze(
-            0).expand((channels, height, width))
+        x = x.view((height, width))
         return x
     elif mode == 'bernoulli':
-        return bernoulli(full((height, width), probability, device=device)).expand((channels, height, width))
+        return bernoulli(full((height, width), probability, device=device))
